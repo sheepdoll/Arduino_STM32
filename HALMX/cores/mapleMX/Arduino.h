e70be16001fbd8791b6464bef7c06a7ab146e34c
Julie Porter <delectradoll@gmail.com>
2015-06-10 20:26:31 -0700

    Setup Variant for STM32CubeMX CMSIS and HAL IO

---
+++ HALMX/cores/mapleMX/Arduino.h
@@ -0,0 +1,25 @@
+/*
+	Stub for main Arduino include
+*/
+
+#ifndef __Arduino_h__
+#define __Arduino_h__
+//#include "WProgram.h"
+
+/* sketch */
+void setup(void);
+void loop(void);
+
+#ifdef __cplusplus
+extern "C"{
+#endif // __cplusplus
+void yield(void);
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+//#include "variant.h"
+
+
+
+#endif
